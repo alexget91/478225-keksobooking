afd3e515a60336695e1a1c20d73dfc38203a8092
Alex <alex.get91@gmail.com>
2017-08-25 16:52:27 +0300

    Добавлено открытие/закрытие всплывающего окна на карте и подгрузка информации в него

--- js/map.js
+++ js/map.js
@@ -42,17 +42,37 @@ var AD_TITLES = [
 var CHECK_IN_TIMES = ['12:00', '13:00', '14:00'];
 var CHECK_OUT_TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+var SIMILAR_OFFER_NUMBER = 8;
+var OFFER_LOCATION_X_MIN = 300;
+var OFFER_LOCATION_X_MAX = 900;
+var OFFER_LOCATION_Y_MIN = 100;
+var OFFER_LOCATION_Y_MAX = 500;
+var OFFER_PRICE_MIN = 1000;
+var OFFER_PRICE_MAX = 1000000;
+var OFFER_ROOMS_MIN = 1;
+var OFFER_ROOMS_MAX = 5;
+var OFFER_GUESTS_MIN = 1;
+var OFFER_GUESTS_MAX = 8;
+var OFFER_FEAUTERS_LENGTH_MIN = 1;
 
 
-// Возвращает случайное целое число между min (включительно) и max (не включая max)
-var getRandomInt = function (min, max) {
+// Возвращает случайное целое число между min (включительно) и max (зависит от includeMax)
+var getRandomInt = function (min, max, includeMax) {
+  if (typeof includeMax !== 'undefined') {
+    max++;
+  }
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
 // Создаёт DOM-элемент для метки на карте
-var createPin = function (ad, fragment) {
+var createPin = function (ad, fragment, id) {
   var div = document.createElement('div');
   div.className = 'pin';
+  if (typeof id !== 'undefined') {
+    div.dataset.id = id;
+  }
   var divStyles = getComputedStyle(div);
   div.style.left = ad.location.x - parseInt(divStyles.width, 10) / 2 + 'px';
   div.style.top = ad.location.y - parseInt(divStyles.height, 10) + 'px';
@@ -61,8 +81,20 @@ var createPin = function (ad, fragment) {
   img.width = 40;
   img.height = 40;
   img.src = ad.author.avatar;
+  img.tabIndex = 0;
   div.appendChild(img);
   fragment.appendChild(div);
+};
+
+var activatePin = function (pin) {
+  pin.classList.add('pin--active');
+};
+
+var deactivatePin = function (map) {
+  var pinActive = map.querySelector('.pin--active');
+  if (pinActive) {
+    pinActive.classList.remove('pin--active');
+  }
 };
 
 // Создаёт всплывающее окно для метки на карте
@@ -83,26 +115,55 @@ var createPinPopup = function (ad, template, dialog) {
   dialogElement.querySelector('.lodge__description').textContent = ad.offer.description;
 
   fragment.appendChild(dialogElement);
-
   dialog.replaceChild(fragment, dialog.querySelector('.dialog__panel'));
   dialog.querySelector('.dialog__title img').src = ad.author.avatar;
 };
 
+var popupDialogShow = function (dialog) {
+  dialog.classList.remove('hidden');
+};
+
+var popupDialogHide = function (dialog) {
+  dialog.classList.add('hidden');
+};
+
+var openPinPopup = function (evt) {
+  var pin = evt.target.tagName === 'IMG' ? evt.target.parentElement : evt.target;
+  deactivatePin(pinMap);
+  activatePin(pin);
+  if (typeof pin.dataset.id !== 'undefined') {
+    createPinPopup(similarAds[pin.dataset.id], dialogPanelContent, dialogPopup);
+    popupDialogShow(dialogPopup);
+  }
+  document.addEventListener('keydown', onPinPopupEscPress);
+};
+
+var closePinPopup = function () {
+  popupDialogHide(dialogPopup);
+  deactivatePin(pinMap);
+  document.removeEventListener('keydown', onPinPopupEscPress);
+};
+
+var onPinPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePinPopup();
+  }
+};
 
 var similarAds = [];
-
-for (var i = 0; i < 8; i++) {
-  var avatarNumber = i < 9 ? '0' + (i + 1) : '' + (i + 1);
+// Формирование массива соседних объявлений
+for (var i = 0; i < SIMILAR_OFFER_NUMBER; i++) {
+  var avatarNumber = i < 9 ? '0' + (i + 1) : '' + (i + 1); // добавление ведущего нуля для чисел < 9
   var titleNumber = getRandomInt(0, AD_TITLES.length);
   var offerLocation = {
-    x: getRandomInt(300, 900),
-    y: getRandomInt(100, 500)
+    x: getRandomInt(OFFER_LOCATION_X_MIN, OFFER_LOCATION_X_MAX, true),
+    y: getRandomInt(OFFER_LOCATION_Y_MIN, OFFER_LOCATION_Y_MAX, true)
   };
   // Получаем массив случайной длины из случайных значений FEATURES (offerFeatures)
-  var featuresLength = getRandomInt(1, FEATURES.length);
+  var featuresLength = getRandomInt(OFFER_FEAUTERS_LENGTH_MIN, FEATURES.length);
   var objFeatures = {};
   while (Object.keys(objFeatures).length < featuresLength) {
-    var item = FEATURES[getRandomInt(1, FEATURES.length)];
+    var item = FEATURES[getRandomInt(OFFER_FEAUTERS_LENGTH_MIN, FEATURES.length)];
     objFeatures[item] = true;
   }
   var offerFeatures = Object.keys(objFeatures);
@@ -114,10 +175,10 @@ for (var i = 0; i < 8; i++) {
     offer: {
       title: AD_TITLES[titleNumber].title,
       address: offerLocation.x + ', ' + offerLocation.y,
-      price: getRandomInt(1000, 1000000),
+      price: getRandomInt(OFFER_PRICE_MIN, OFFER_PRICE_MAX, true),
       type: AD_TITLES[titleNumber].type,
-      rooms: getRandomInt(1, 6),
-      guests: getRandomInt(1, 9),
+      rooms: getRandomInt(OFFER_ROOMS_MIN, OFFER_ROOMS_MAX, true),
+      guests: getRandomInt(OFFER_GUESTS_MIN, OFFER_GUESTS_MAX, true),
       checkin: CHECK_IN_TIMES[getRandomInt(0, CHECK_IN_TIMES.length)],
       checkout: CHECK_OUT_TIMES[getRandomInt(0, CHECK_OUT_TIMES.length)],
       features: offerFeatures,
@@ -128,14 +189,38 @@ for (var i = 0; i < 8; i++) {
   };
 }
 
+// Отображение соседних объектов на карте
 var fragment = document.createDocumentFragment();
 for (i = 0; i < similarAds.length; i++) {
-  createPin(similarAds[i], fragment);
+  createPin(similarAds[i], fragment, i);
 }
 var pinMap = document.querySelector('.tokyo__pin-map');
 pinMap.appendChild(fragment);
 
+
 var dialogPanelTemplate = document.querySelector('#lodge-template');
 var dialogPanelContent = dialogPanelTemplate.content ? dialogPanelTemplate.content : dialogPanelTemplate;
 var dialogPopup = document.querySelector('#offer-dialog');
-createPinPopup(similarAds[0], dialogPanelContent, dialogPopup);
+var dialogPopupClose = dialogPopup.querySelector('.dialog__close');
+
+pinMap.addEventListener('click', function (evt) {
+  openPinPopup(evt);
+});
+
+pinMap.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPinPopup(evt);
+  }
+});
+
+dialogPopupClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  closePinPopup();
+});
+
+dialogPopupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    evt.preventDefault();
+    closePinPopup();
+  }
+});
