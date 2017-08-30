a78b1b4e60cb79f9bbcf4f8391f05fa1e6602de8
Alex <alex.get91@gmail.com>
2017-08-30 18:37:44 +0300

    Добавлены универсальные функции showCard и synchronizeFields

--- js/card.js
+++ js/card.js
@@ -3,33 +3,9 @@
 // Модуль для работы со всплывающим окном на карте
 (function () {
 
-  var dialogPanelTemplate = document.querySelector('#lodge-template');
-  var dialogPanelContent = dialogPanelTemplate.content ? dialogPanelTemplate.content : dialogPanelTemplate;
   var dialogPopup = document.querySelector('#offer-dialog');
   var dialogPopupClose = dialogPopup.querySelector('.dialog__close');
   var pinMap = document.querySelector('.tokyo__pin-map');
-
-  // Создаёт всплывающее окно для метки на карте
-  var createPinPopup = function (ad, template, dialog) {
-    var fragment = document.createDocumentFragment();
-    var dialogElement = template.cloneNode(true);
-
-    dialogElement.querySelector('.lodge__title').textContent = ad.offer.title;
-    dialogElement.querySelector('.lodge__address').textContent = ad.offer.address;
-    dialogElement.querySelector('.lodge__price').innerHTML = ad.offer.price + '&#x20bd;/ночь';
-    dialogElement.querySelector('.lodge__type').textContent = window.data.OFFER_TYPES[ad.offer.type].name;
-    dialogElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + ad.offer.guests + ' гостей в ' + ad.offer.rooms + ' комнатах';
-    dialogElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
-    var feautersElement = dialogElement.querySelector('.lodge__features');
-    for (var i = 0; i < ad.offer.features.length; i++) {
-      feautersElement.innerHTML += '<span class="feature__image feature__image--' + ad.offer.features[i] + '"></span>';
-    }
-    dialogElement.querySelector('.lodge__description').textContent = ad.offer.description;
-
-    fragment.appendChild(dialogElement);
-    dialog.replaceChild(fragment, dialog.querySelector('.dialog__panel'));
-    dialog.querySelector('.dialog__title img').src = ad.author.avatar;
-  };
 
   var popupDialogShow = function (dialog) {
     dialog.classList.remove('hidden');
@@ -44,7 +20,7 @@
     window.pin.deactivatePin(pinMap);
     window.pin.activatePin(pin);
     if (typeof pin.dataset.id !== 'undefined') {
-      createPinPopup(window.data.similarAds[pin.dataset.id], dialogPanelContent, dialogPopup);
+      window.showCard(pin.dataset.id);
       popupDialogShow(dialogPopup);
     }
     document.addEventListener('keydown', onPinPopupEscPress);
@@ -80,4 +56,5 @@
   pinMap.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, openPinPopup);
   });
+
 })();
---
+++ js/show-card.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Показывает карточку выбранного жилья по нажатию на метку на карте
+(function () {
+
+  window.showCard = function (id) {
+    var dialogPanelTemplate = document.querySelector('#lodge-template');
+    var dialogPanelContent = dialogPanelTemplate.content ? dialogPanelTemplate.content : dialogPanelTemplate;
+    var dialogElement = dialogPanelContent.cloneNode(true);
+    var dialogPopup = document.querySelector('#offer-dialog');
+    var fragment = document.createDocumentFragment();
+    var ad = window.data.similarAds[id];
+
+    dialogElement.querySelector('.lodge__title').textContent = ad.offer.title;
+    dialogElement.querySelector('.lodge__address').textContent = ad.offer.address;
+    dialogElement.querySelector('.lodge__price').innerHTML = ad.offer.price + '&#x20bd;/ночь';
+    dialogElement.querySelector('.lodge__type').textContent = window.data.OFFER_TYPES[ad.offer.type].name;
+    dialogElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + ad.offer.guests + ' гостей в ' + ad.offer.rooms + ' комнатах';
+    dialogElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+    var feautersElement = dialogElement.querySelector('.lodge__features');
+    for (var i = 0; i < ad.offer.features.length; i++) {
+      feautersElement.innerHTML += '<span class="feature__image feature__image--' + ad.offer.features[i] + '"></span>';
+    }
+    dialogElement.querySelector('.lodge__description').textContent = ad.offer.description;
+
+    fragment.appendChild(dialogElement);
+    dialogPopup.replaceChild(fragment, dialogPopup.querySelector('.dialog__panel'));
+    dialogPopup.querySelector('.dialog__title img').src = ad.author.avatar;
+  };
+
+})();
